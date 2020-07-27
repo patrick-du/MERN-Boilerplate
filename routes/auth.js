92894088c430caaa3019e7ed3caac0e113c4adf4
Patrick Du <patrickduu@gmail.com>
2020-07-27 00:35:59 -0400

    Add imports, initialize middlewares, setup MongoDB

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+const Joi = require('@hapi/joi');
+
+const schema = {
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+};
+
+router.post('/register', async (req, res) => {
+
+  Joi.validate(req, body, schema);
+  
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,72 @@
-require('dotenv').config();
-
+// Server Imports
 const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+// Authentication Imports
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+// Route Imports
+const authRoute = require('./routes/auth');
+
+// DB Imports
+const mongoose = require('mongoose');
+
+// Initialize dotenv
+dotenv.config();
+
+// Environment Varibles
+const PORT = process.env.PORT || 5000;
+const DB_URI = process.env.DB_URI;
+
+
+// To do --------------------------------------------------------
+// Mock DB (replace with MongoDB using Mongoose later)
+// - User data from email-password
+// - User data from OAuth has no password
+const DATA = [
+  { email: 'test@gmail.com', password: '1234' },
+  { email: 'test2@gmail.com', provider: 'facebook' },
+];
+
+// Connect to DB
+// mongoose.connect(
+//   DB_URI,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   () => {
+//     console.log('Connected to DB!');
+//   },
+// );
 
 const app = express();
 
-// Environment Varibles
-let port = process.env.PORT || 8080;
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(passport.initialize());
 
-// Simple '/' Route
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// // Route Middlewares
+// app.use('/api/user', authRoute);
+
+// Config
+const config = { secretOrKey: 'mysecret' };
+
+
+// Utility Functions
+const findOrCreate = () => {};
+
+const checkUser = () => {};
 
 // Server
-app.listen(port, () => {
-  console.log(`Server is listening on localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on localhost:${PORT}`);
 });
